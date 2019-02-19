de611b73a24de84a6c43895bd263aed75619e467
VaiciukasE <47764575+VaiciukasE@users.noreply.github.com>
2019-02-19 04:27:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,59 @@
-const Discord = require('discord.js');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
 
-const client = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 
- 
+bot.on("ready" , async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setGame("discord.io/BEPROTNAMIS!")
+});
 
-client.on('ready', () => {
+bot.on("message" , async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
 
-    console.log('I am ready!');
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}pagalba`){
+    return message.channel.send("Administratoriams žinutė išsiusta , susisieksime su jumis kaip galėsime");
+  }
+
+if(cmd === `${prefix}informacija`){
+
+  let sicon = message.guild.iconURL;
+  let serverembed = new Discord.RichEmbed()
+  .setDescription("Serverio Informacija")
+  .setColor("#3399ff")
+  .setThumbnail(sicon)
+  .addField("Serverio Pavadinimas" , message.guild.name)
+  .addField("Sukurimo Data" , message.guild.createdAt)
+  .addField("Tu prisijungei" , message.member.joinedAt)
+  .addField("Visi Nariai" , message.guild.memberCount);
+
+  return message.channel.send(serverembed);
+
+}
+
+
+if(cmd === `${prefix}apiebota`){
+
+  let bicon = bot.user.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
+  .setDescription("Boto Informacija")
+  .setColor("#3399ff")
+  .setThumbnail(bicon)
+  .addField("Boto Vardas" , bot.user.username)
+  .addField("Sukurimo Data" , bot.user.createdAt);
+
+
+  return message.channel.send(botembed);
+
+}
+
 
 });
 
- 
-
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NTQ3MTU3MTQ5OTE5MjE1NjE4.D0zzGQ.2eGkON1OuPjlmsyl1qlVnHOzpHw);//where BOT_TOKEN is the token of our bot
+bot.login(botconfig.token);
